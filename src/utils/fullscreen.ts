28b67f40cb87e585cb3004993ac4d346b231c91a
Rafael Oto <rafaelotoso@gmail.com>
2024-07-15 16:34:49 -0300

    feat: screenful inclusion

--- src/utils/fullscreen.ts
+++ src/utils/fullscreen.ts
@@ -1,29 +1,16 @@
-export default function toggleFullScreen() {
-  const elem = document.documentElement;
-  if (
-    !document.fullscreenElement &&
-    !document.mozFullScreenElement &&
-    !document.webkitFullscreenElement &&
-    !document.msFullscreenElement
-  ) {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { // Firefox
-      elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { // IE/Edge
-      elem.msRequestFullscreen();
+import screenfull from 'screenfull';
+
+export function isFullScreenEnabled() {
+  return screenfull.isEnabled
+}
+
+export function toggleFullScreen() {
+  if (screenfull.isEnabled) {
+    if (!screenfull.isFullscreen) {
+      screenfull.request()
     }
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) { // Firefox
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { // IE/Edge
-      document.msExitFullscreen();
+    if (screenfull.isFullscreen) {
+      screenfull.exit()
     }
   }
 }
